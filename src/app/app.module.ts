3c6ebf03a56547b0a42a6d04c9bd61ae5e19185c
jay.paragas.ext <jay.paragas.ext@inetum.com>
2023-07-03 11:01:45 +0200

    Primera version

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+//Modulos Angular Materials
+import { SharedModule } from './shared/shared.module';
+
+//Componentes
+import { AgregarEditarCountryComponent } from './components/agregar-editar-country/agregar-editar-country.component';
+import { ListaCountryComponent } from './components/lista-country/lista-country.component';
+import { VerCountryComponent } from './components/ver-country/ver-country.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AgregarEditarCountryComponent,
+    ListaCountryComponent,
+    VerCountryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
