2b0dde02465f5fed34e3c4221e16bc13d76f8826
veronica.cartagena <cartagenaveronica.19@gmail.com>
2023-07-17 11:52:29 +0200

    pendiente

---
+++ Models/Users.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.Models
+{
+    public class Users 
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UsesLastName { get; set; }
+        public string UserPhone { get; set; }  
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+
+        public String[] Roles { get; set; }
+    }
+}
