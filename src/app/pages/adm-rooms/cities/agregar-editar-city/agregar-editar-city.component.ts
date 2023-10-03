89ff48123787106ac3ca85b05c6e7b0caca91bd1
RidgeSecure <137873971+RidgeSecure@users.noreply.github.com>
2023-10-03 10:21:47 +0200

    desplegable city

--- src/app/pages/adm-rooms/cities/agregar-editar-city/agregar-editar-city.component.ts
+++ src/app/pages/adm-rooms/cities/agregar-editar-city/agregar-editar-city.component.ts
@@ -1,100 +1,106 @@
-import { Component, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { ActivatedRoute, Router } from '@angular/router';
-import { City } from '../../../../interfaces/city';
-import { CityService } from '../../../../services/city.service';
+  import { Component, Inject } from '@angular/core';
+  import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+  import { MatSnackBar } from '@angular/material/snack-bar';
+  import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+  import { ActivatedRoute, Router } from '@angular/router';
+  import { City } from '../../../../interfaces/city';
+  import { CityService } from '../../../../services/city.service';
+  import { CountryService } from '../../../../services/country.service';
+  import { Country } from '../../../../interfaces/country';
 
-@Component({
-  selector: 'app-agregar-editar-city',
-  templateUrl: './agregar-editar-city.component.html',
-  styleUrls: ['./agregar-editar-city.component.css']
-})
-export class AgregarEditarCityComponent {
-  loading: boolean = false;
-  form: FormGroup
-  cityId: number;
-  Operacion: string = 'Add';
-
-
-  constructor(
-    @Inject(MAT_DIALOG_DATA) private data: any,
-    private dialogRef: MatDialogRef<AgregarEditarCityComponent>,
-    private _cityService: CityService,
-    private fb: FormBuilder,
-    private _snackBar: MatSnackBar,
-    private router: Router,
-    private aRoute: ActivatedRoute) {
-    this.form = this.fb.group({
-      cityName: ['', Validators.required], ////Campo requerido
-      countryId: ['', Validators.required]
-    })
-
-    this.cityId = data.identification;
-  }
+  @Component({
+    selector: 'app-agregar-editar-city',
+    templateUrl: './agregar-editar-city.component.html',
+    styleUrls: ['./agregar-editar-city.component.css']
+  })
+  export class AgregarEditarCityComponent {
+    loading: boolean = false;
+    form: FormGroup
+    cityId: number;
+    Operacion: string = 'Add';
+    countries?: Country[]  ;
 
 
-  ngOnInit(): void {
-    if (this.cityId != 0) {
-      this.Operacion = 'Editar';
-      this.obtenerCity(this.cityId);
-    }
-  }
 
-  obtenerCity(cityId: number) {
-    this.loading = true;
-    this._cityService.getCity(this.cityId).subscribe(data => {
-      this.form.setValue({
-        cityName: data.cityName,
-        countryId: data.countryId
+    constructor(
+      @Inject(MAT_DIALOG_DATA) private data: any,
+      private dialogRef: MatDialogRef<AgregarEditarCityComponent>,
+      private _cityService: CityService,
+      private fb: FormBuilder,
+      private _snackBar: MatSnackBar,
+      private router: Router,
+      private _countryService: CountryService,
+      private aRoute: ActivatedRoute) {
+      this.form = this.fb.group({
+        cityName: ['', Validators.required], 
+        countryId: ['', Validators.required],
+        countryName:['', Validators.required]
       })
-      this.loading = false;
-    });
-  }
 
-  //Metodos
-  agregarEditarCity() {
-    //Definir el objeto
-    const city: City = {
-      cityName: this.form.value.cityName,
-      countryId: this.form.value.countryId
+      this.cityId = data.identification;
     }
 
-    if (this.cityId != 0) {
-      city.cityId = this.cityId;
-      this.editarcity(this.cityId, city);
-    } else {
-      this.agregarcity(city);
+
+    ngOnInit(): void {
+      if (this.cityId != 0) {
+        this.Operacion = 'Editar';
+        this.obtenerCity(this.cityId);
+      }
+      this._countryService.getCountries().subscribe(data => this.countries = data);
     }
-  }
 
-  editarcity(cityId: number, city: City) {
-    this.loading = true;
-    this._cityService.updateCity(cityId, city).subscribe(data => {
-      this.loading = false;
-      this.mensajeExito('actualizada');
-      this.router.navigate(['/home/admRooms/cities/listaCity']);
-    })
-  }
+    obtenerCity(cityId: number) {
+      this.loading = true;
+      this._cityService.getCity(this.cityId).subscribe(data => {
+        this.form.patchValue({
+          cityName: data.cityName,
+        })
+        this.loading = false;
+      });
+    }
 
-  agregarcity(city: City) {
-    //Enivar el objeto al backend
-    this._cityService.addCity(city).subscribe(data => {
-      this.mensajeExito('registrado');
-      this.router.navigate(['/home/admRooms/cities/listaCity']);
-    });
-  }
+    //Metodos
+    agregarEditarCity() {
+      //Definir el objeto
+      const city: City = {
+        cityName: this.form.value.cityName,
+        countryId: this.form.value.countryId
+      }
 
-  mensajeExito(texto: string) {
-    this._snackBar.open(`La ciudad fue ${texto} con éxito`, '', {
-      duration: 4000,
-      horizontalPosition: 'right'
-    });
-  }
+      if (this.cityId != 0) {
+        city.cityId = this.cityId;
+        this.editarcity(this.cityId, city);
+      } else {
+        this.agregarcity(city);
+      }
+    }
 
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
+    editarcity(cityId: number, city: City) {
+      this.loading = true;
+      this._cityService.updateCity(cityId, city).subscribe(data => {
+        this.loading = false;
+        this.mensajeExito('actualizada');
+        this.router.navigate(['/home/admRooms/cities/listaCity']);
+      })
+    }
 
-}
+    agregarcity(city: City) {
+      //Enivar el objeto al backend
+      this._cityService.addCity(city).subscribe(data => {
+        this.mensajeExito('registrado');
+        this.router.navigate(['/home/admRooms/cities/listaCity']);
+      });
+    }
+
+    mensajeExito(texto: string) {
+      this._snackBar.open(`La ciudad fue ${texto} con éxito`, '', {
+        duration: 4000,
+        horizontalPosition: 'right'
+      });
+    }
+
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
+
+  }
