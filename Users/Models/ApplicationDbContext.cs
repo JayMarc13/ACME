e2002744ef0bf3daf02ecfabce1e827f5b44140c
jay.paragas.ext <jay.paragas.ext@inetum.com>
2023-10-05 12:31:57 +0200

    API Users

---
+++ Users/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Users.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Aqui es donde haces las foreign keys o algo
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityUserRole<string>>()
+                .HasKey(ur => new { ur.UserId, ur.RoleId });
+
+            modelBuilder.Entity<IdentityUserLogin<string>>()
+                .HasKey(l => new { l.LoginProvider, l.ProviderKey });
+
+            modelBuilder.Entity<IdentityUserToken<string>>()
+              .HasKey(t => new { t.UserId, t.LoginProvider, t.Name });
+        }
+    }
+}
--- Users/Program.cs
+++ Users/Program.cs
@@ -1,6 +1,56 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Users.Models;
+using Users.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
+//Conexión a la base de datos
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetSection("ConnectionStrings:DefaultConnection").Value);
+});
+
+//Configuración del identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    // Configuración del password
+}).AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddRoles<IdentityRole>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddTransient<IAuthService, AuthService>();
+
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddUserManager<UserManager<IdentityUser>>();
+
+
+//Configuración del token
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateActor = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        RequireExpirationTime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration.GetSection("Jwt:Issuer").Value,
+        ValidAudience = builder.Configuration.GetSection("Jwt:Audience").Value,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Jwt:Key").Value))
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,5 +71,33 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var Name = "Administrador";
+    // comprueba si existe
+    if (!await roleManager.RoleExistsAsync(Name))
+    {
+        var role = new IdentityRole(Name);
+        await roleManager.CreateAsync(role);
+    }
+
+    //correo de administrador super adm
+    var usuario = await userManager.FindByEmailAsync("adm@acme.com");
+
+
+    if (usuario != null)
+    {
+        var isInRole = await userManager.IsInRoleAsync(usuario, Name);
+
+        if (!isInRole)
+        {
+            await userManager.AddToRoleAsync(usuario, Name);
+        }
+    }
+}
 
 app.Run();
